950101d64da474d3df736dccafa09cf9b839261b
shivam-jainn <shivam.clgstash@gmail.com>
2024-10-10 13:26:17 -0400

    (+) adyenplatform : transform : PoEligibility : payoutResponse

--- crates/router/src/connector/adyenplatform/transformers.rs
+++ crates/router/src/connector/adyenplatform/transformers.rs
@@ -37,6 +37,12 @@ pub struct AdyenPlatformRouterData<T> {
     pub router_data: T,
 }
 
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct Amount {
+    pub currency: storage_enums::Currency,
+    pub value: MinorUnit,
+}
+
 impl<T> TryFrom<(MinorUnit, T)> for AdyenPlatformRouterData<T> {
     type Error = Report<errors::ConnectorError>;
     fn try_from((amount, item): (MinorUnit, T)) -> Result<Self, Self::Error> {
@@ -68,4 +74,92 @@ pub struct PayoutCardDetails {
     expiry_month: Secret<String>,
     expiry_year: Secret<String>,
     holder_name: Secret<String>,
+}
+
+
+#[cfg(feature = "payouts")]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdyenPayoutResponse {
+    psp_reference: String,
+    result_code: Option<AdyenStatus>,
+    response: Option<AdyenStatus>,
+    amount: Option<Amount>,
+    merchant_reference: Option<String>,
+    refusal_reason: Option<String>,
+    refusal_reason_code: Option<String>,
+    additional_data: Option<AdditionalData>,
+    auth_code: Option<String>
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum AdyenStatus {
+    AuthenticationFinished,
+    AuthenticationNotRequired,
+    Authorised,
+    Cancelled,
+    ChallengeShopper,
+    Error,
+    IdentifyShopper,
+    PartiallyAuthorised,
+    Pending,
+    Received,
+    RedirectShopper,
+    Refused,
+    PresentToShopper,
+    #[cfg(feature = "payouts")]
+    #[serde(rename = "[payout-confirm-received]")]
+    PayoutConfirmReceived,
+    #[cfg(feature = "payouts")]
+    #[serde(rename = "[payout-decline-received]")]
+    PayoutDeclineReceived,
+    #[cfg(feature = "payouts")]
+    #[serde(rename = "[payout-submit-received]")]
+    PayoutSubmitReceived,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub enum AdyenRecurringModel {
+    Subscription,
+    UnscheduledCardOnFile,
+    CardOnFile,
+}
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+pub enum AuthType {
+    #[default]
+    PreAuth,
+    FinalAuth
+}
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdditionalData {
+    authorisation_type: Option<AuthType>,
+    manual_capture: Option<String>,
+    execute_three_d: Option<String>,
+    pub recurring_processing_model: Option<AdyenRecurringModel>,
+    /// Enable recurring details in dashboard to receive this ID, https://docs.adyen.com/online-payments/tokenization/create-and-use-tokens#test-and-go-live
+    #[serde(rename = "recurring.recurringDetailReference")]
+    recurring_detail_reference: Option<Secret<String>>,
+    #[serde(rename = "recurring.shopperReference")]
+    recurring_shopper_reference: Option<String>,
+    network_tx_reference: Option<Secret<String>>,
+    #[cfg(feature = "payouts")]
+    payout_eligible: Option<PayoutEligibility>,
+    funds_availability: Option<String>,
+}
+
+pub enum PayoutEligibility {
+    #[serde(rename = "Y")]
+    Yes,
+    #[serde(rename = "N")]
+    #[default]
+    No,
+    #[serde(rename = "D")]
+    Domestic,
+    #[serde(rename = "U")]
+    Unknown,
 }
